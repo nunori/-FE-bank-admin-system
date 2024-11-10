bc7c851d04366023f5fb3133f1d6a0421f5023aa
이재성 <commonhm@gmail.com>
2024-11-10 12:35:16 +0900

    fix: home redirect setting

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,10 @@ import BranchLayoutView from "@/views/BranchLayoutView.vue";
 
 const routes = [
   {
+    path: "/",
+    redirect: "/home",
+  },
+  {
     path: "/home",
     name: "Home",
     component: HomeView,
@@ -40,7 +44,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes,
 });
 
